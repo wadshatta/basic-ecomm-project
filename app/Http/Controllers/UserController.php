5e29a72175123871e850a2bc41c6f67c45f4d4f7
fakhareldin abakar <39832576+wadshatta@users.noreply.github.com>
2023-07-01 11:24:17 +0500

    second push i make a few things like middelware and paly with sessions and create product view and login view with controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+      $user = User::where(['email'=> $request->email])->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)){
+            return "Username or password is not correct.";
+        }else{
+            $request->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,19 @@
 <div class="container custom-login">
     <div class="row justify-content-center">
         <div class="col-sm-4">
-            <form>
+            <form action="login" method="POST">
+                @csrf
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
             
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
               </form>
         </div>
     </div>
